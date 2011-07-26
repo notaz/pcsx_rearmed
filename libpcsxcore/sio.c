aa35134a834ff7e98b5a474c3db399a3d660da22
notaz <notasas@gmail.com>
2011-07-26 19:45:03 +0300

    sio: only do IRQ if it's bit has been cleared
    tested on the real thing.

--- libpcsxcore/sio.c
+++ libpcsxcore/sio.c
@@ -386,8 +386,10 @@ void sioInterrupt() {
 	PAD_LOG("Sio Interrupt (CP0.Status = %x)\n", psxRegs.CP0.n.Status);
 #endif
 //	SysPrintf("Sio Interrupt\n");
-	StatReg |= IRQ;
-	psxHu32ref(0x1070) |= SWAPu32(0x80);
+	if (!(StatReg & IRQ)) {
+		StatReg |= IRQ;
+		psxHu32ref(0x1070) |= SWAPu32(0x80);
+	}
 }
 
 void LoadMcd(int mcd, char *str) {
