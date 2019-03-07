0963774720493115d123104f9ddc506d49a886bc
retro-wertz <retro-wertz@users.noreply.github.com>
2019-03-07 18:36:22 +0800

    Run SysReset() only from retro_run, causes retroarch to freeze... ... on some platforms

--- frontend/libretro.c
+++ frontend/libretro.c
@@ -1289,7 +1289,10 @@ bool retro_load_game(const struct retro_game_info *info)
 		return false;
 	}
 
-	SysReset();
+	/* TODO: Calling SysReset() outside retro_run for some system
+	 * causes RetroArch to freeze, e.g Ludo */
+	//SysReset();
+	rebootemu = 1;
 
 	if (LoadCdrom() == -1) {
 		log_cb(RETRO_LOG_INFO, "could not load CD\n");
