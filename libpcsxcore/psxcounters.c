d3cf89a1d127a21a64d2ef2bd1947f30e77be485
Paul Cercueil <paul@crapouillou.net>
2019-09-13 23:01:57 +0200

    Make sure hardware registers are manipulated as little-endian
    The hardware registers should be represented in little-endian format.
    Therefore, on big-endian systems the values need to be byte-swapped.
    
    Signed-off-by: Paul Cercueil <paul@crapouillou.net>

--- libpcsxcore/psxcounters.c
+++ libpcsxcore/psxcounters.c
@@ -328,7 +328,7 @@ void psxRcntUpdate()
         // VSync irq.
         if( hSyncCount == VBlankStart )
         {
-            HW_GPU_STATUS &= ~PSXGPU_LCF;
+            HW_GPU_STATUS &= SWAP32(~PSXGPU_LCF);
             GPU_vBlank( 1, 0 );
             setIrq( 0x01 );
 
@@ -348,9 +348,9 @@ void psxRcntUpdate()
             frame_counter++;
 
             gpuSyncPluginSR();
-            if( (HW_GPU_STATUS & PSXGPU_ILACE_BITS) == PSXGPU_ILACE_BITS )
-                HW_GPU_STATUS |= frame_counter << 31;
-            GPU_vBlank( 0, HW_GPU_STATUS >> 31 );
+            if ((HW_GPU_STATUS & SWAP32(PSXGPU_ILACE_BITS)) == SWAP32(PSXGPU_ILACE_BITS))
+                HW_GPU_STATUS |= SWAP32(frame_counter << 31);
+            GPU_vBlank(0, SWAP32(HW_GPU_STATUS) >> 31);
         }
 
         // Schedule next call, in hsyncs
