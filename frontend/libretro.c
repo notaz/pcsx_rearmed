1d579bf754d091727cb9b2ae0e6933b256739b59
negativeExponent <negativeExponent@users.noreply.github.com>
2020-05-25 16:04:39 +0800

    Fix interpreter-only build (DYNAREC=0)
    - For systems that can only run in interpreter mode for whatever reasons.

--- frontend/libretro.c
+++ frontend/libretro.c
@@ -59,18 +59,6 @@
 
 #define INTERNAL_FPS_SAMPLE_PERIOD 64
 
-#ifdef DRC_DISABLE
-int stop;
-u32 next_interupt;
-u32 event_cycles[PSXINT_COUNT];
-int cycle_multiplier;
-int new_dynarec_hacks;
-
-void new_dyna_before_save(void) {}
-void new_dyna_after_save(void) {}
-void new_dyna_freeze(void *f, int i) {}
-#endif
-
 //hack to prevent retroarch freezing when reseting in the menu but not while running with the hot key
 static int rebootemu = 0;
 
