8a0521eef6fe31b9db78b1fef1c64aa49ef8f9f9
notaz <notasas@gmail.com>
2022-09-10 02:37:50 +0300

    fix build
    forgot to add 'extern'

--- libpcsxcore/psxinterpreter.h
+++ libpcsxcore/psxinterpreter.h
@@ -6,6 +6,6 @@ void execI();
 void intApplyConfig();
 void MTC0(psxRegisters *regs_, int reg, u32 val);
 void gteNULL(struct psxCP2Regs *regs);
-void (*psxCP2[64])(struct psxCP2Regs *regs);
+extern void (*psxCP2[64])(struct psxCP2Regs *regs);
 
 #endif // __PSXINTERPRETER_H__
