cddc7ca96ddefe7acabac845b4ec99cf72b3e6fc
Paul Cercueil <paul@crapouillou.net>
2023-02-11 14:47:30 +0000

    Support compiling without posix_memalign() function
    Add NO_POSIX_MEMALIGN option in Makefile.libretro to support platform
    that don't provide the posix_memalign() function.
    
    Signed-off-by: Paul Cercueil <paul@crapouillou.net>

--- frontend/libretro.c
+++ frontend/libretro.c
@@ -3037,7 +3037,7 @@ void retro_init(void)
 
 #ifdef _3DS
    vout_buf = linearMemAlign(VOUT_MAX_WIDTH * VOUT_MAX_HEIGHT * 2, 0x80);
-#elif defined(_POSIX_C_SOURCE) && (_POSIX_C_SOURCE >= 200112L) && !defined(VITA) && !defined(__SWITCH__)
+#elif defined(_POSIX_C_SOURCE) && (_POSIX_C_SOURCE >= 200112L) && HAVE_POSIX_MEMALIGN
    if (posix_memalign(&vout_buf, 16, VOUT_MAX_WIDTH * VOUT_MAX_HEIGHT * 2) != 0)
       vout_buf = (void *) 0;
 #else
