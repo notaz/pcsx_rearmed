016c6e93f6db684211f5c8b05433cb500715ba50
notaz <notasas@gmail.com>
2024-02-08 00:26:40 +0200

    libretro: adjust psxclock description
    libretro/pcsx_rearmed#820

--- frontend/libretro_core_options.h
+++ frontend/libretro_core_options.h
@@ -200,9 +200,9 @@ struct retro_core_option_v2_definition option_defs_us[] = {
 #endif
    {
       "pcsx_rearmed_psxclock",
-      "PSX CPU Clock Speed",
+      "PSX CPU Clock Speed (%)",
       NULL,
-      "Overclock or under-clock the PSX CPU. Try adjusting this if the game is too slow, too fast or hangs."
+      "Overclock or under-clock the PSX CPU. The value has to be lower than 100 because of some slowdowns (cache misses, hw access penalties, etc.) that are not emulated. Try adjusting this if the game is too slow, too fast or hangs."
 #if defined(HAVE_PRE_ARMV7) && !defined(_3DS)
       " Default is 50."
 #else
