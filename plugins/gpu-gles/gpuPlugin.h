3259ae18d6f89c6a003c7ba62b2469a1c1c0ad06
notaz <notasas@gmail.com>
2025-02-14 01:15:27 +0200

    unbreak gh build

--- plugins/gpu-gles/gpuPlugin.h
+++ plugins/gpu-gles/gpuPlugin.h
@@ -55,6 +55,7 @@ extern "C" {
 
 #define bool unsigned short
 
+#if 0
 typedef struct {
 	unsigned int ulFreezeVersion;
 	unsigned int ulStatus;
@@ -62,7 +63,6 @@ typedef struct {
 	unsigned char psxVRam[1024*1024*2];
 } GPUFreeze_t;
 
-#if 0
 long CALLBACK GPUinit();
 long CALLBACK GPUshutdown();
 long CALLBACK GPUopen(unsigned long *disp, char *cap, char *cfg);
