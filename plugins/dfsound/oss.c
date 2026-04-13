591b7a04cc64fcf1ef9ef32228b804777b72bb67
notaz <notasas@gmail.com>
2026-04-14 02:01:45 +0300

    deal with some gcc15 warnings

--- plugins/dfsound/oss.c
+++ plugins/dfsound/oss.c
@@ -25,6 +25,10 @@
 #include <sys/soundcard.h>
 #include "out.h"
 
+#if defined(__GNUC__) && (__GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 5))
+#pragma GCC diagnostic ignored "-Wunused-result"
+#endif
+
 ////////////////////////////////////////////////////////////////////////
 // oss globals
 ////////////////////////////////////////////////////////////////////////
